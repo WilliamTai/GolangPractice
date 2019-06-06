1df3250445d29f18a09dc8937b3240590385764d
WilliamTai <>
2019-06-06 11:22:14 +0900

    change: using if {...} else {...} condition and try go fmt

--- 2.Control flow/main.go
+++ 2.Control flow/main.go
@@ -11,12 +11,11 @@ func main() {
 	fmt.Println("Ex1:")
 	fmt.Print("for INIT; CONDITION; POST{}:")
 	for i := 0; i < 10; i++ {
-		fmt.Print(i,",")
+		fmt.Print(i, ",")
 	}
 	//output:
 	//Ex1:
 	//simple loop:0,1,2,3,4,5,6,7,8,9,
-
 
 	//there are no Do loop in Golang but you can do with for condition or for {}
 	//this is useful when build a TCP listener or something else need to run all the time
@@ -25,16 +24,15 @@ func main() {
 	fmt.Print("z % 3 == 0 output:")
 	z := 0
 	for z < 10 {
-		if z % 3 ==0{
-			fmt.Print(z," ")
+		if z%3 == 0 {
+			fmt.Print(z, " ")
 		}
 		z++
 	}
 	//output:
-    //Ex2:
+	//Ex2:
 	//for condition {}
 	//z % 3 == 0 output:0 3 6 9
-
 
 	//for {} example
 
@@ -45,15 +43,14 @@ func main() {
 	x := 0
 	for {
 		x++
-		if x % 2 == 0{
-			fmt.Print(x,",")
-		}
-
 		//put break condition here to prevent infinity loop
-		if x > 20{
+		if x > 20 {
 			fmt.Println("\nx = ", x, ",break!")
 			break
+		} else if x%2 == 0 {
+			fmt.Print(x, ",")
 		}
+
 	}
 	//output
 	//Ex3:
