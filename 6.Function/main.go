e957af800f51a56e2bf42ea5d2e1dbd498486811
WilliamTai <>
2019-06-20 20:51:56 +0900

    add: variadic parameter

--- 6.Function/main.go
+++ 6.Function/main.go
@@ -22,6 +22,16 @@ func main() {
 	fmt.Println("Name: ", name, "Flag: ", flag)
 	//output:
 	//Name:  WilliamTai Flag:  true
+
+	printVariadicParameter(2, 3, 4, 5, 6)
+	//output:
+	//	[2 3 4 5 6]
+	//[]int
+	//Loop, now is:  0 Adding:  2 sum:  2
+	//Loop, now is:  1 Adding:  3 sum:  5
+	//Loop, now is:  2 Adding:  4 sum:  9
+	//Loop, now is:  3 Adding:  5 sum:  14
+	//Loop, now is:  4 Adding:  6 sum:  20
 }
 
 func printHelloWorld() {
@@ -40,4 +50,15 @@ func setFullFillString(s string) string {
 
 func getTwoReturnValue(firstName, lastName string) (string, bool) {
 	return fmt.Sprint(firstName, lastName), true
+}
+
+func printVariadicParameter(x ...int){
+	fmt.Println(x)
+	fmt.Printf("%T\n", x)
+
+	sum := 0
+	for i, v := range x {
+		sum += v
+		fmt.Println("Loop, now is: ", i, "Adding: ", v, "sum: ", sum)
+	}
 }
